2f6d6f6212021bf1ad77df7ba8b69fe294bdd22c
Pro-Sultan <sultanabaniks@gmail.com>
2024-07-17 21:34:14 +0100

    Added some configurations in the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      grey: "#BFBFBF",
+      black06: "#0F0F0F",
+      black10: "#1A1A1A",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
+    backgroundImage: {
+        'navbar-head-image': "url('./src/assets/images/StreamVibe-Home-Background.png')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
     extend: {},
   },
   plugins: [],
