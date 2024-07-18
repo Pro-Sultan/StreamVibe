8db54fbe968e31a3ddd0e7c70954378056875dfb
Pro-Sultan <sultanabaniks@gmail.com>
2024-07-18 01:57:22 +0100

    JS file for the links

---
+++ Navlinks.js
@@ -0,0 +1,22 @@
+
+const Navlinks = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Movies&Shows",
+    href: "/movies",
+  },
+  {
+    label: "Support",
+    href: "/support",
+  },
+  {
+    label: "Subscription",
+    href: "/subscription",
+  },
+
+];
+
+export default Navlinks
