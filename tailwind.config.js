8546a476d82782759f01c723ff6a0d39ce05c603
Pro-Sultan <sultanabaniks@gmail.com>
2024-07-18 01:59:41 +0100

    Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,19 @@ export default {
       bermuda: "#78dcca",
     },
     backgroundImage: {
-        'navbar-head-image': "url('./src/assets/images/StreamVibe-Home-Background.png')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      },
+      "navbar-head-image":
+        "url('./src/assets/images/StreamVibe-Home-Background.png')",
+    },
+    screens: {
+      tablet: "-800px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
