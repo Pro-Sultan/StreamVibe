953df78fbd35143eef8cc9b1b78204e9d39a65a4
Pro-Sultan <sultanabaniks@gmail.com>
2024-07-24 09:58:40 +0100

    Added a preloader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,32 @@ import Explore from "../Components/Explore";
 import Navbar from "../Components/Navbar";
 import NavbarTitle from "../Components/NavbarTitle";
 import Devices from "../Components/Devices";
+import Preloader from "../Components/Preloader"; // Create a new Preloader component
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 3000); // Simulate a 2-second delay
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <div className="others">
-        <NavbarTitle />
-        <Explore />
-        <Devices />
-      </div>
+      {!loaded ? (
+        <Preloader />
+      ) : (
+        <div>
+          <Navbar />
+          <div className="others">
+            <NavbarTitle />
+            <Explore />
+            <Devices />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
